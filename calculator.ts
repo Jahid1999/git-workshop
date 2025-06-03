c8d87ef02d615ac6504f22b5e74cdec4282f0379
Abdullah Al Jahid <46520236+Jahid1999@users.noreply.github.com>
2025-06-03 21:07:37 +0600

    Update calculator.ts

--- calculator.ts
+++ calculator.ts
@@ -3,3 +3,9 @@ add(num1: number, num2: number): number { return num1 + num2; }
 subtract(num1: number, num2: number): number { return num1 - num2; }
 
 multiply(num1: number, num2: number): number { return num1 * num2; }
+
+swap (a, b) {
+  a = a + b;
+  a = a-b;
+  a = a-b;
+}
