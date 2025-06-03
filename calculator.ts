750440c1571e59a11b83a0f5df86c3a5443c52b8
Jahid <abdullah.jahid@streamstech.com>
2025-06-03 21:07:14 +0600

    divide added

--- calculator.ts
+++ calculator.ts
@@ -3,3 +3,5 @@ add(num1: number, num2: number): number { return num1 + num2; }
 subtract(num1: number, num2: number): number { return num1 - num2; }
 
 multiply(num1: number, num2: number): number { return num1 * num2; }
+
+divide(num1: number, num2: number): number { return num1 / num2; }
