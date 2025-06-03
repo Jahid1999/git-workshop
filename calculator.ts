b1e9ed5c12e22bb751e8412030fec4ea4ca40ae2
Jahid <abdullah.jahid@streamstech.com>
2025-06-03 20:58:05 +0600

    add multiply

--- calculator.ts
+++ calculator.ts
@@ -1,3 +1,5 @@
 add(num1: number, num2: number): number { return num1 + num2; }
 
 subtract(num1: number, num2: number): number { return num1 - num2; }
+
+multiply(num1: number, num2: number): number { return num1 * num2; }
